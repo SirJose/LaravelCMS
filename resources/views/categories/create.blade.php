dd1d3039c841c8996847598d62084b2da4eccdfc
Jose <josecl@galileo.edu>
2020-01-27 00:03:56 -0600

    Store categories.

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+  <div class="card">
+    <div class="card-header">
+      Create category
+    </div>
+
+    <div class="card-body">
+
+      @if ($errors->any())
+        <div class="alert alert-danger">
+          <ul class="list-group">
+            @foreach ($errors->all() as $error)
+              <li class="list-group-item text-danger">{{$error}}</li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
+
+      <form action="{!! route('categories.store') !!}" method="POST">
+        @csrf
+
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" name="name" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <button class="btn btn-success" type="submit" name="button">Add Category</button>
+        </div>
+
+      </form>
+
+    </div>
+
+  </div>
+
+@endsection
